0ef04aa24086989b2cf5835194f29dd77ad5ff3a
uma shankar <umashankar.kousha@gmail.com>
2026-03-15 19:34:14 +0530

    initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hi Welcome to Your workout Tracker
+      </h1>
+    </>
+  );
+}
+
+export default App;
